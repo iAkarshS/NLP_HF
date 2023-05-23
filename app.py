34467f657ec58a706947936665de9dbd9cda5140
iAkarshS <51397893+iAkarshS@users.noreply.github.com>
2023-05-23 22:50:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ import string
 import streamlit as st
 import webbrowser
 global Lrdetect_Model
+global filename
+
+from datetime import datetime
+
 
 Lrdetect_Model=open('testmodel.pckl','rb')
 Lrdetect_Model=pickle.load(Lrdetect_Model)
 
 st.title("Emotion detection")
 input_test=st.text_input("input text", '')
+t=1
 button_clicked=st.button("get emotion")
 if(button_clicked):
-	st.text(Lrdetect_Model.predict([input_test]))
+    st.text(Lrdetect_Model.predict([input_test]))
+    with open("log.txt","a") as out_file:
+        if t==1:
+            now = datetime.now()
+            os=str(now)+","+input_test+","+str(Lrdetect_Model.predict([input_test]))+"\n"
+            out_file.write(os)
+            t=0
